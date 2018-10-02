6d0b399274ee99dad6b618496a0574998046af19
Vu Le Nam <namvule@hotmail.com>
2018-10-02 14:08:02 +0900

    append qrcode module

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,12 @@
 from flask import render_template
-
+from .qrcode import bp, make_qrcode_url
 from app import app
+
+app.config['SECRET_KEY'] = 'random-bytes'
+app.register_blueprint(bp)
 
 @app.route('/')
 @app.route('/index')
 def index():
-  user = {'username': 'Miguel'}
-  return render_template('index.html', title='Home', user=user)
+  qrcode_url = make_qrcode_url('https://linoxide.com/linux-how-to/install-flask-python-ubuntu/')
+  return render_template('index.html', title="Home", qrcode_url=qrcode_url)
