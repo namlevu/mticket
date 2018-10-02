6b7d5d8e30e95a5bd933bcf7cc1d2dce07cfec84
Vu Le Nam <namvule@hotmail.com>
2018-10-02 15:04:48 +0900

    append create ticket form

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,7 @@
 from flask import render_template, request
 from .qrcode import bp, make_qrcode_url
+import base64
+
 from app import app
 
 app.config['SECRET_KEY'] = 'abc'
@@ -9,7 +11,15 @@ app.register_blueprint(bp)
 @app.route('/index', methods=['GET', 'POST'])
 def index():
   if request.method == 'POST':
-    qrcode_url = make_qrcode_url('http://www.msn.com')
+    base_url = "http://www.namvl.com/"
+    email = request.form['email']
+    tel = request.form['tel']
+    ticket_quanlity = request.form['quanlity']
+    bought_at = '2018/10/10'
+    amount = request.form['amount']
+    key = email + '&' + tel + '&' + ticket_quanlity + '&' + bought_at + '&' + amount
+    url = base_url + key
+    qrcode_url = make_qrcode_url(url)
     return render_template('index.html', title="Home", qrcode_url=qrcode_url)
 
   if request.method == 'GET':
