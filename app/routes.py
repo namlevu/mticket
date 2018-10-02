5c7920c31e369d12f31ac667d715f4eb400bccc5
Vu Le Nam <namvule@hotmail.com>
2018-10-02 14:41:58 +0900

    test get/post method

--- app/routes.py
+++ app/routes.py
@@ -1,12 +1,20 @@
-from flask import render_template
+from flask import render_template, request
 from .qrcode import bp, make_qrcode_url
 from app import app
 
-app.config['SECRET_KEY'] = 'random-bytes'
+app.config['SECRET_KEY'] = 'abc'
 app.register_blueprint(bp)
 
-@app.route('/')
-@app.route('/index')
+@app.route('/', methods=['GET', 'POST'])
+@app.route('/index', methods=['GET', 'POST'])
 def index():
-  qrcode_url = make_qrcode_url('https://linoxide.com/linux-how-to/install-flask-python-ubuntu/')
+  qrcode_url = make_qrcode_url('http://www.namvl.com')
+  if request.method == 'POST':
+    qrcode_url = make_qrcode_url('http://www.msn.com')
+  if request.method = 'GET':
+    qrcode_url = make_qrcode_url('http://www.google.com')
   return render_template('index.html', title="Home", qrcode_url=qrcode_url)
+
+@app.route('/checkin')
+def checkin():
+  return ""
