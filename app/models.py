73faf8665692281c154b679f58deb6ddeedb8959
Vu Le Nam <namvule@hotmail.com>
2018-10-02 15:52:11 +0900

    db.Integer takes no parameters

--- app/models.py
+++ app/models.py
@@ -13,7 +13,7 @@ class Ticket(db.Model):
   id = db.Column(db.Integer, primary_key=True)
   buyer_email = db.Column(db.String(128), index=True, unique=True)
   buyer_tel = db.Column(db.String(16))
-  quanlity = db.Column(db.Integer(16))
+  quanlity = db.Column(db.Integer)
   bought_at = db.Column(db.DateTime())
   amount = db.Column(db.Integer)
 
